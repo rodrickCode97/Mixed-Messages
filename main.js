fc39c5171edb0eee1da7e0c9c11b638e5308056f
RJZ3 <rodrickCode@gmail.com>
2022-10-27 18:51:44 -0500

    First Project commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// creating an mixed message program that displays the your future day 
+// getting user's input 
+const getUserInput = userInput =>{
+    userInput = userInput.toLowerCase();
+}
+
+// arrays of future days of the week 
+
+futureDays = ["monday", "tuesday", "wednesday", "thursday", "friday", 'Thursday'];
+
+//  array's for sayings 
+
+saying1 = [];
+saying2 = [];
+saying3 = [];
+
+// set indexes for sayings
+saying1[0] = 'tables';
+saying1[1] = "apples";
+saying1[2] = "weekend plans";
+saying1[3] = "your job";
+saying2[0] = "your bestfriend";
+saying2[1] = "yogurt";
+saying2[2] = "shows";
+saying2[3] = "spending money";
+saying3[0] = "your family";
+saying3[1] = "your friends";
+saying3[2] = "video games"
+saying3[3] = "dogs";
+
+
+// set up a function with a conditional statement for the input and the output 
+
+const mixedM = (UserInput, arr) => {
+    if(userInput === futureDays[i])
+    }
+} 
