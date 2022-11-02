651a79af730165b08b0eebc896a7727cb66f2529
RJZ3 <rodrickCode@gmail.com>
2022-11-01 21:48:46 -0500

    Mixed Messages V1.0.0

--- main.js
+++ main.js
@@ -1,48 +1,16 @@
 // creating an mixed message program that displays the your future day 
-
+// Require the prompt module to be able to grab the users input 
+const prompt = require('prompt-sync')();
 // Message prompting the user to write a message.
-// console.log('Please enter the day of the week to know what you should attempt, do and avoid.')
-
-// function to get user's input 
-function getUserInput(userInput) {
-    userInput = userInput.toLowerCase();
-    switch (userInput) {
-        case monday:
-            return 'monday';
-        case tuesday:
-            return 'tuesday';
-        case wednesday:
-            return 'wednesday';
-        case thursday:
-            return 'thursday';
-        case friday:
-            return 'friday';
-        case saturday:
-            return 'saturday';
-        case sunday:
-            return 'sunday';
-    }
-}
+const date = prompt('Welcome to the future day teller, please enter a weekday you would like to know about. ')
 
 //  array's for sayings 
 
-saying1 = [];
-saying2 = [];
-saying3 = [];
+saying1 = ["tables", "apples","weekend plans", "your job"];
+saying2 = ["your bestfriend", "yougurt", "TV shows", "spending money"];
+saying3 = ["your family", "your friends", "video games", "dogs"];
 
-// set indexes for sayings
-saying1[0] = 'tables';
-saying1[1] = "apples";
-saying1[2] = "weekend plans";
-saying1[3] = "your job";
-saying2[0] = "your bestfriend";
-saying2[1] = "yogurt";
-saying2[2] = "shows";
-saying2[3] = "spending money";
-saying3[0] = "your family";
-saying3[1] = "your friends";
-saying3[2] = "video games"
-saying3[3] = "dogs";
+
 // set variables to output random elements of the 3 arrays 
 
 let randomSaying1 = saying1[Math.floor(Math.random()*4)];
@@ -50,15 +18,15 @@ let randomSaying2 = saying2[Math.floor(Math.random()*4)];
 let randomSaying3 = saying3[Math.floor(Math.random()*4)];
 
 // set up a function that will output the future message
-const mixedM = getUserInput =>{
-console.log("You've chose " + getUserInput);
- if(getUserInput){
-console.log(`On ${getUserInput} you should avoid ${randomSaying1}`);
+const mixedM = () => {
+console.log("You've chose " + date + "!");
+ if(date){
+console.log(`On ${date} you should avoid ${randomSaying1}`);
 console.log(`You should stay away from ${randomSaying2}`);
-console.log(`also You should stay around ${randomSaying3}`);
+console.log(` and also You should stay around ${randomSaying3}`);
  } else {
     console.log('Error');
  }
 };
 
-mixedM(getUserInput);
+mixedM();
